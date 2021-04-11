5e15b2f62e27ebcfad3fef2d1092c244a6d28c34
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-11 07:19:48 +0300

    Add function Greeting

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,9 +1,18 @@
-import React from 'react'
-import ReactDom from 'react-dom'
+import React from 'react';
+import ReactDom from 'react-dom';
 
 
-function Greeting(){
-  return <h4>Good Morning!</h4>;
+// const Greeting = () => {
+//   return React.createElement('h4', {}, 'Good Morning ^_^!')
+// };
+
+
+function Greeting() {
+  return( 
+    <div>
+      <h4>Good Morning ^_^!</h4>
+    </div>
+  );
 }
 
 ReactDom.render(<Greeting/>, document.getElementById('root'));
