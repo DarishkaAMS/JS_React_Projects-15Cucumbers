8aa85fbace48d999f423c9ddfdcbfb3b2bff767a
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-12 06:44:12 +0300

    Add JSX Javascript

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -12,10 +12,7 @@ function BookList() {
   return( 
     <React.Fragment>
       <section className="booklist">
-
-        <h4>This is a BookList</h4>
-        <Book/>
-        <Book/>
+        
         <Book/>
 
       </section>
@@ -23,20 +20,23 @@ function BookList() {
   );
 }
 
+const author = "DarishkaAMS"
 const Book = () => {
-  return <article className="book">
-    This is my favourite book
-      <Author/> 
-      <br/>
-      <Image/>
-  </article>;
+  const title = "Python Cookbook";
+  return (
+    <article className="book">
+      <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--FaVyk1l0--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://images-na.ssl-images-amazon.com/images/I/51hFtDvqgfL._SX379_BO1%2C204%2C203%2C200_.jpg" alt="#" width="200" height="250"/>
+      <h3>{author.toUpperCase()}</h3>
+      <h4>{title}</h4>
+    </article>
+  );
 };
 
-const Author = () => {
-  return <h4 style={{color: "#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}> I have written this book </h4>;
-};
+// const Author = () => {
+//   return <h4 style={{color: "#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}> I have written this book </h4>;
+// };
 
-const Image = () => <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--FaVyk1l0--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://images-na.ssl-images-amazon.com/images/I/51hFtDvqgfL._SX379_BO1%2C204%2C203%2C200_.jpg" alt="#" width="200" height="250"/>;
+// const Image = () => ();
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
