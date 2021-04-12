f89a615d3832aa36f50eced91e890535ae29bafe
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-12 05:30:41 +0300

    Add img and author

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -14,6 +14,8 @@ function BookList() {
 
         <h4>This is a BookList</h4>
         <Book/>
+        <Author/>
+        <Image/>
 
       </section>
     </React.Fragment>
@@ -25,7 +27,12 @@ const Book = () => {
     This is my favourite book
   </article>;
 };
-// const Message = {} => 
+
+const Author = () => {
+  return <b> I have written this book </b>;
+};
+
+const Image = () => <img src="https://rimage.gnst.jp/livejapan.com/public/article/detail/a/00/01/a0001976/img/basic/a0001976_main.jpg?20210118175750&q=80&rw=750&rh=536" alt="#"/>;
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
