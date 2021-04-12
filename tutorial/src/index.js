cf4ab02cae9c00e6c488b2500f58a24b03a28358
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-12 06:21:15 +0300

    Add JSX Css styling

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -33,10 +33,10 @@ const Book = () => {
 };
 
 const Author = () => {
-  return <b> I have written this book </b>;
+  return <h4 style={{color: "#617d98", fontSize: '0.75rem', marginTop: '0.25rem'}}> I have written this book </h4>;
 };
 
-const Image = () => <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--FaVyk1l0--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://images-na.ssl-images-amazon.com/images/I/51hFtDvqgfL._SX379_BO1%2C204%2C203%2C200_.jpg" alt="#"/>;
+const Image = () => <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--FaVyk1l0--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://images-na.ssl-images-amazon.com/images/I/51hFtDvqgfL._SX379_BO1%2C204%2C203%2C200_.jpg" alt="#" width="200" height="250"/>;
 
 
 ReactDom.render(<BookList/>, document.getElementById('root'));
