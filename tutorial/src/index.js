f9e83ee9cc780c38994f923abb7bc35ba082e3bf
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-12 07:15:25 +0300

    Add props

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -1,33 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+//CSS
 import './index.css'
 
-// const Greeting = () => {
-//   return React.createElement('h4', {}, 'Good Morning ^_^!')
-// };
+// Setup vars
+const firstBook = {
+  image: "https://res.cloudinary.com/practicaldev/image/fetch/s--FaVyk1l0--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://images-na.ssl-images-amazon.com/images/I/51hFtDvqgfL._SX379_BO1%2C204%2C203%2C200_.jpg",
+  title: "Python Cookbook",
+  author: "DarishkaAMS"
+}
 
+const secondBook = {
+  image: "https://images-na.ssl-images-amazon.com/images/I/71oUHJ6uO7L.jpg",
+  title: "JS: The Definitive Guide",
+  author: "DarishkaAMS"
+}
 
 function BookList() {
   return( 
     <React.Fragment>
       <section className="booklist">
         
-        <Book/>
+        <Book image={firstBook.image} title={firstBook.title} author={firstBook.author}/>
+        <Book image={secondBook.image} title={secondBook.title} author={secondBook.author}/>
 
       </section>
     </React.Fragment>
   );
 }
 
-const author = "DarishkaAMS"
-const Book = () => {
-  const title = "Python Cookbook";
+
+const Book = (props) => {
   return (
     <article className="book">
-      <img src="https://res.cloudinary.com/practicaldev/image/fetch/s--FaVyk1l0--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://images-na.ssl-images-amazon.com/images/I/51hFtDvqgfL._SX379_BO1%2C204%2C203%2C200_.jpg" alt="#" width="200" height="250"/>
-      <h3>{author.toUpperCase()}</h3>
-      <h4>{title}</h4>
+      <img src={props.image} alt="#" width="200" height="250"/>
+      <h3>{props.author}</h3>
+      <h4>{props.title}</h4>
     </article>
   );
 };
