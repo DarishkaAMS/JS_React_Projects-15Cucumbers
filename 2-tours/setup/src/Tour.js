0493e6ac448a2fd4727151c4eb8be550c4092f47
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-20 06:23:56 +0300

    Add Tours Display Data

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react';
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({id, image, info, price, name}) => {
+
+  return (
+  <article className='single-tour'>
+    <img src={image} alt={name}/>
+    <footer>
+      <div className='tour-info'>
+        <h4>{name}</h4>
+        <h4 className="tour-price">$ {price} </h4>
+        <br/>
+      </div>
+      <p>
+        <h5>{info}</h5>
+      </p>
+      <button className="delete-btn">
+        Maybe next time
+      </button>
+    </footer>
+    </article>
+    );
 };
 
 export default Tour;
