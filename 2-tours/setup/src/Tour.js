b9df90a5de4d707eb1e3346a6aa082ef95eb6223
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-20 06:43:53 +0300

    Complete Tours project

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({id, image, info, price, name}) => {
+const Tour = ({id, image, info, price, name, removeTour}) => {
   const [readMore, setReadMore] = useState(false)
 
 
@@ -19,7 +19,7 @@ const Tour = ({id, image, info, price, name}) => {
         {readMore ? 'Show Less' : 'Read more'}
       </button>
       </p>
-      <button className="delete-btn">
+      <button className="delete-btn" onClick={()=>removeTour(id)}>
         Maybe next time
       </button>
     </footer>
