31bde3c64941c0a1de815b78b1f365cbd1507c53
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-20 06:31:56 +0300

    Add Tours Toggle Button

--- 2-tours/setup/src/Tour.js
+++ 2-tours/setup/src/Tour.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, image, info, price, name}) => {
+  const [readMore, setReadMore] = useState(false)
+
 
   return (
   <article className='single-tour'>
@@ -11,8 +13,11 @@ const Tour = ({id, image, info, price, name}) => {
         <h4 className="tour-price">$ {price} </h4>
         <br/>
       </div>
-      <p>
-        <h5>{info}</h5>
+      <p> {readMore ? info: `${info.substring(0,200)}...
+      `} 
+      <button onClick={()=> setReadMore(!readMore)}>
+        {readMore ? 'Show Less' : 'Read more'}
+      </button>
       </p>
       <button className="delete-btn">
         Maybe next time
