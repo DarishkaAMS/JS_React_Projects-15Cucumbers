6b923c671a5af0460922b33f2266e37ebf01b2aa
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-04-21 06:51:05 +0300

    Add React Icons

--- 3-reviews/setup/src/App.js
+++ 3-reviews/setup/src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import Review from './Review';
+import { FaCat } from 'react-icons/fa';
+
+
 function App() {
-  return <h2>reviews project setup</h2>;
+  return (
+  <h2>
+    reviews project setup
+    <FaCat className="icon"/>
+    </h2>
+  );
 }
 
 export default App;
